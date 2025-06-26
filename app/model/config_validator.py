7ea1cec719283f75a4b2c856dc494b9e4d8c5c66
Bhavani Sri Alekhya <152136977+PBS-Alekhya@users.noreply.github.com>
2025-06-26 11:48:59 +0530

    Delete app/model/config_validator.py

--- app/model/config_validator.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, ValidationError, conlist
-from typing import List
-
-class ConfigSchema(BaseModel):
-    model_name: str
-    version: str
-    confidence_threshold: float
-    labels: conlist(str, min_length=2, max_length=2)
-    log_predictions: bool
